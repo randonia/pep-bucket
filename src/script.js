aa80fd7163f6739b96cda1985848ac361854876e
Ryan Andonian <ryan.andonian@declara.com>
2014-09-19 18:29:01 -0700

    Refactored the process_udiff to be foreach friendly, and made the call use an anonymous function that directly calls it

--- src/script.js
+++ src/script.js
@@ -12,25 +12,35 @@ function create_linecount_div(char_count, style_classes){
     return new_element;
 }
 
-function process_diff_container(element, index, array){
-    // Grab all the additions, deletions, and common lines separately
-    var additions = element.getElementsByClassName('udiff-line addition');
-    var deletions = element.getElementsByClassName('udiff-line deletion');
-    var commons = element.getElementsByClassName('udiff-line common');
-
-    for(var i = 0; i < additions.length; ++i){
-	var gutter = additions[i].getElementsByClassName('gutter')[0];
-	var span_element = additions[i].getElementsByClassName('source')[0];
+function process_udiff(element, all_elements, line_starter){
+    for(var i = 0; i < all_elements.length; ++i){
+	var gutter = all_elements[i].getElementsByClassName('gutter')[0];
+	var span_element = all_elements[i].getElementsByClassName('source')[0];
 	var span_content = span_element.innerText;
-	if(span_content[0] != '+'){
+	if(span_content[0] != line_starter){
 	    console.log("Something is wrong");
 	}
 	span_content = span_content.substring(1);
 	var char_count = span_content.length;
 	var style = (char_count > 100)?LINE_ERROR:LINE_CLEAN;
 	var new_element = create_linecount_div(char_count, style);
-	additions[i].insertBefore(new_element, span_element);
+	all_elements[i].insertBefore(new_element, span_element);
     }
+}
+
+function process_diff_container(element, index, array){
+    // Grab all the additions, deletions, and common lines separately
+    var additions = element.getElementsByClassName('udiff-line addition');
+    var deletions = element.getElementsByClassName('udiff-line deletion');
+    var commons = element.getElementsByClassName('udiff-line common');
+
+    var context = {
+	'line_starter': '+'
+    };
+
+    additions.forEach(function(element, index, array){
+	process_udiff(element, array, this['line_starter']);
+    }, context);
 }
 
 
