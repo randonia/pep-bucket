ed768efe45a1bd2e58dd5933fda41b44856a5f44
Ryan Andonian <ryan.andonian@declara.com>
2014-09-19 18:07:34 -0700

    Refactored to pull each diff section.

--- src/myscript.js
+++ src/myscript.js
@@ -1,3 +1,8 @@
+var LINE_ERROR = 'pep-column-count error';
+var LINE_CLEAN = 'pep-column-count clean';
+
+HTMLCollection.prototype.forEach = Array.prototype.forEach;
+
 // Creates the counting div element on the document and returns
 function create_linecount_div(char_count, style_classes){
     var new_element = document.createElement('div');
@@ -7,25 +12,27 @@ function create_linecount_div(char_count, style_classes){
     return new_element;
 }
 
-var LINE_ERROR = 'pep-column-count error';
-var LINE_CLEAN = 'pep-column-count clean';
+function process_diff_container(element, index, array){
+    // Grab all the additions, deletions, and common lines separately
+    var additions = element.getElementsByClassName('udiff-line addition');
+    var deletions = element.getElementsByClassName('udiff-line deletion');
+    var commons = element.getElementsByClassName('udiff-line common');
 
-// Grab all the additions, deletions, and common lines separately
-var additions = document.getElementsByClassName('udiff-line addition');
-var deletions = document.getElementsByClassName('udiff-line deletion');
-var commons = document.getElementsByClassName('udiff-line common');
-
-for(var i = 0; i < additions.length; ++i){
-    var gutter = additions[i].getElementsByClassName('gutter')[0];
-    var span_element = additions[i].getElementsByClassName('source')[0];
-    var span_content = span_element.innerText;
-    if(span_content[0] != '+'){
-	console.log("Something is wrong");
+    for(var i = 0; i < additions.length; ++i){
+	var gutter = additions[i].getElementsByClassName('gutter')[0];
+	var span_element = additions[i].getElementsByClassName('source')[0];
+	var span_content = span_element.innerText;
+	if(span_content[0] != '+'){
+	    console.log("Something is wrong");
+	}
+	span_content = span_content.substring(1);
+	var char_count = span_content.length;
+	var style = (char_count > 100)?LINE_ERROR:LINE_CLEAN;
+	var new_element = create_linecount_div(char_count, style);
+	additions[i].insertBefore(new_element, span_element);
     }
-    span_content = span_content.substring(1);
-    var char_count = span_content.length;
-
-    var style = (char_count > 100)?LINE_ERROR:LINE_CLEAN;
-    var new_element = create_linecount_div(char_count, style);
-    additions[i].insertBefore(new_element, span_element);
 }
+
+
+var all_diff_containers = document.getElementsByClassName('diff-container');
+all_diff_containers.forEach(process_diff_container);
